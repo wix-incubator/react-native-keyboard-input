44bff9170980eb951eb1bf895b017421e8ab4112
Artal Druk <artald@wix.com>
2017-02-28 19:51:07 +0200

    support storing params in the registry (to hold name, icon, etc)

--- src/KeyboardsRegistry.js
+++ src/KeyboardsRegistry.js
@@ -3,13 +3,13 @@ import {AppRegistry} from 'react-native';
 export default class KeyboardRegistry {
   static registeredKeyboards = {};
 
-  static registerComponent = (conponentID, generator) => {
-    KeyboardRegistry.registeredKeyboards[conponentID] = generator;
+  static registerComponent = (conponentID, generator, params = {}) => {
+    KeyboardRegistry.registeredKeyboards[conponentID] = {generator, params, conponentID};
     AppRegistry.registerComponent(conponentID, generator);
   }
 
   static getComponent = (conponentID) => {
-    const generator = KeyboardRegistry.registeredKeyboards[conponentID];
+    const {generator} = KeyboardRegistry.registeredKeyboards[conponentID];
     if (!generator) {
       console.error(`KeyboardRegistry.getComponent: ${conponentID} used but not yet registered`);
       return undefined;
