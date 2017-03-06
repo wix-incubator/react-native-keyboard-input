131d9e06b40513215f0baf5ad0517e2c6dc0e427
Amit Davidi <amit.d4vidi@gmail.com>
2017-03-06 21:12:53 +0200

    Android: Move module to library project

--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -22,7 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
+      return Arrays.asList(
           new MainReactPackage(),
           new RNKeyboardInputPackage()
       );
