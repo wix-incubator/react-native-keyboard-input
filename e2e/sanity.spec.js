197c701e434d307a79b1a24bb4d1698c05a7eff4
Artal Druk <artald@wix.com>
2017-05-08 10:49:38 +0300

    restore e2e sanity, should now work with detox 5.0.7 or later

--- e2e/sanity.spec.js
+++ e2e/sanity.spec.js
@@ -10,15 +10,18 @@ describe('Sanity', () => {
   it('should switch between keyboards', async () => {
     await element(by.id('input')).tap();
     await element(by.label('show1')).tap();
+    await expect(element(by.label('HELOOOO!!!'))).toBeVisible();
     await element(by.label('show2')).tap();
+    await expect(element(by.label('*** ANOTHER ONE ***'))).toBeVisible();
     await element(by.label('reset')).tap();
     await expect(element(by.label('Keyboards example'))).toBeVisible();
   });
 
-  //This test times-out at the moment since detox can't find elements in the keyboard window
-  //it('should show the first demo keyboard', async () => {
-  //  await element(by.id('input')).tap();
-  //  await element(by.label('show1')).tap();
-  //  await expect(element(by.label('HELOOOO!!!'))).toBeVisible();
-  //});
+  it('should select item on the first demo keyboard', async () => {
+    await element(by.id('input')).tap();
+    await element(by.label('show1')).tap();
+    await expect(element(by.label('HELOOOO!!!'))).toBeVisible();
+    await element(by.label('Click Me!')).tap();
+    await expect(element(by.id('demo-message'))).toBeVisible();
+  });
 });
