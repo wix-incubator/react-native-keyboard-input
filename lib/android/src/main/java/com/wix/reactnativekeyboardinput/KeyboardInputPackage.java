d3e27ec776f856001e26e90a7e8f23db682b7532
Guy Carmeli <guyc@wix.com>
2018-01-04 10:16:51 +0200

    Deprecate createJSModules

--- lib/android/src/main/java/com/wix/reactnativekeyboardinput/KeyboardInputPackage.java
+++ lib/android/src/main/java/com/wix/reactnativekeyboardinput/KeyboardInputPackage.java
@@ -40,7 +40,7 @@ public class KeyboardInputPackage implements ReactPackage {
         return Arrays.<ViewManager>asList(new CustomKeyboardRootViewManager(mLayout));
     }
 
-    @Override
+    // Deprecated in RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
