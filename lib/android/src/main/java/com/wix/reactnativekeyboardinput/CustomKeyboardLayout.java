f0d8841b8cb42ab048b90be865bfb3cf1d4fc897
nguyen95 <36287542+nguyen95@users.noreply.github.com>
2022-07-13 15:04:43 +0700

    Update CustomKeyboardLayout.java

--- lib/android/src/main/java/com/wix/reactnativekeyboardinput/CustomKeyboardLayout.java
+++ lib/android/src/main/java/com/wix/reactnativekeyboardinput/CustomKeyboardLayout.java
@@ -1,7 +1,9 @@
 package com.wix.reactnativekeyboardinput;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
